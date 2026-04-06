7cfcbb5ae2a2c1b76824705292ade6293d4856d1
Renzo Bojorquez <u202411402@upc.edu.pe>
2026-04-06 12:22:17 -0500

    feat(supplier): add supplier aggregate with identifier and name properties

---
+++ ACME.OOP/SCM/Domain/Model/Aggregates/Supplier.cs
@@ -0,0 +1,10 @@
+using oopsample.Shared.Domain.Model.ValueObjects;
+
+namespace oopsample.SCM.Domain.Model.Aggregates;
+
+public class Supplier(string identifier, string name, Address address)
+{
+    public string Id = identifier ?? throw new ArgumentNullException(nameof(identifier));
+    public string Name { get; init; } = name ?? throw new ArgumentNullException(nameof(name));
+    public Address Address { get; init; } = address ?? throw new ArgumentNullException(nameof(address));
+}
